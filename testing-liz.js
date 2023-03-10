4a77fc8a39b0a1ae2da3f9c9a792f3fe00ca24c9
Liz <elizabethnino813@elizabeths-mbp.attlocal.net>
2023-03-09 20:35:15 -0600

    Added button classes for Weather API

--- testing-liz.js
+++ testing-liz.js
@@ -2,6 +2,8 @@ var leftBtn = $("#btnOne");
 var rightBtn = $("#btnTwo");
 var imgEl = document.createElement("img");
 var mainEl = document.getElementById("main")
+var btnSearchEl = document.getElementById("btnSearch")
+var userInput = $("#search")
 
 function imageInfo() {
     imgEl.setAttribute("src", "./testing-images/image-1.jpg");
@@ -43,3 +45,23 @@ rightBtn.on("click", function() {
         console.log("Line 43 works")
     }
 })
+
+function getApiSearch() {
+    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput.val() + "&appid=62b78fd432f4f7c034943f7f3abc8315&units=imperial";
+  // Search bar
+    fetch(requestUrl)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+  
+    for(let i = 0; i < data.result.length; i++) {
+      var mainUl = document.createElement('li');
+      mainUl.innerHTML = data.result[i].main;
+      mainUl.appendChild(mainEl);
+      }
+      console.log("Received!!");
+    }
+
+    btnSearchEl.addEventListener("click", function() {
+        getApiSearch()
+        
+      })
